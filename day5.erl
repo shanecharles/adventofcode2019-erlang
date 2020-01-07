bbd483148cbed3727142ddfd6d64c06145ed478a
Shane Charles <shanecharles@burningicesolutions.com>
2020-01-06 21:48:05 -0600

    Add day 5 part 2

--- day5.erl
+++ day5.erl
@@ -1,10 +1,10 @@
 -module(day5).
--export([part1/0, get_opcode/1, run_program/1]).
+-export([part1/0, part2/0, get_opcode/1, run_program/2]).
 
-run_program(Program) when is_list(Program) -> 
+run_program(Program, Input) when is_list(Program) -> 
   P = intcode:get_program("inputs/day5.txt"), 
-  run_program(P) ;
-run_program(Program) -> run_code_acc(0, Program, []).
+  run_program(P, Input) ;
+run_program(Program, Input) -> run_code_acc({0, Program}, Input, []).
 
 get_parameter_mode(X,Div) -> 
   X div Div rem 10.
@@ -20,34 +20,81 @@ get_value({0, Id}, Program) ->
 get_value({1, Id}, Program) -> 
   maps:get(Id, Program).
 
-op_add([A1, A2, {_, Si} | _], Program) -> 
+op_add(Id, [A1, A2, {_, Si} | _], Program) -> 
   V1 = get_value(A1, Program), 
   V2 = get_value(A2, Program),
   S  = maps:get(Si, Program),                          
-  maps:update(S, V1 + V2, Program).
+  {Id+4, maps:update(S, V1 + V2, Program)}.
 
-op_multiply([A1, A2, {_, Si} | _], Program) -> 
+op_multiply(Id, [A1, A2, {_, Si} | _], Program) -> 
   V1 = get_value(A1, Program), 
   V2 = get_value(A2, Program),
   S  = maps:get(Si, Program),                          
-  maps:update(S, V1 * V2, Program).
+  {Id+4, maps:update(S, V1 * V2, Program)}.
 
-op_store(V1,[{_,Si} | _], Program) -> 
+op_store(Id, V1,[{_,Si} | _], Program) -> 
   S = maps:get(Si, Program),
-  maps:update(S, V1, Program).
+  {Id+2, maps:update(S, V1, Program)}.
+
+op_jump_true(Id, [A1, S2 | _], Program) ->
+  V1 = get_value(A1, Program), 
+  Next_id = if
+              V1 =/= 0 -> get_value(S2, Program) ;
+              true     -> Id + 3
+            end,
+  {Next_id, Program}.
+
+op_jump_false(Id, [A1, S2 | _], Program) ->
+  V1 = get_value(A1, Program), 
+  Next_id = if
+              V1 =:= 0 -> get_value(S2, Program) ;
+              true     -> Id + 3
+            end,
+  {Next_id, Program}.
+
+op_less_than(Id, [A1, A2, {_, S1} | _], Program) ->
+  V1 = get_value(A1, Program), 
+  V2 = get_value(A2, Program),
+  S = maps:get(S1, Program),
+  Store =
+    if
+      V1 < V2 -> 1 ;
+      true    -> 0
+    end,
+  {Id+4, maps:update(S, Store, Program)}.
+
+op_equals(Id, [A1, A2, {_, S1} | _], Program) ->
+  V1 = get_value(A1, Program), 
+  V2 = get_value(A2, Program),
+  S = maps:get(S1, Program),
+  Store =
+    if
+      V1 =:= V2 -> 1 ;
+      true      -> 0
+    end,
+  {Id+4, maps:update(S, Store, Program)}.
 
 
-run_code_acc(Id, Program, Output) -> 
+
+run_code_acc({Id, Program}, Input, Output) -> 
   { Op, Modes } = get_opcode(maps:get(Id, Program)),
   [{_, Si} | _] = Args = lists:zip(Modes, lists:map(fun (X) -> X + Id end, [1,2,3])),
   case Op of
     99 -> {Program, lists:reverse(Output) } ;
-     1 -> run_code_acc(Id+4, op_add(Args, Program),Output) ;
-     2 -> run_code_acc(Id+4, op_multiply(Args, Program), Output) ;
-     3 -> run_code_acc(Id+2, op_store(1, Args, Program), Output) ;
-     4 -> run_code_acc(Id+2, Program, [maps:get(maps:get(Si, Program), Program) | Output])
+     1 -> run_code_acc(op_add(Id, Args, Program), Input, Output) ;
+     2 -> run_code_acc(op_multiply(Id, Args, Program), Input, Output) ;
+     3 -> run_code_acc(op_store(Id, Input, Args, Program), Input, Output) ;
+     4 -> run_code_acc({Id+2, Program}, Input, [maps:get(maps:get(Si, Program), Program) | Output]) ;
+     5 -> run_code_acc(op_jump_true(Id, Args, Program), Input, Output) ;
+     6 -> run_code_acc(op_jump_false(Id, Args, Program), Input, Output) ;
+     7 -> run_code_acc(op_less_than(Id, Args, Program), Input, Output) ;
+     8 -> run_code_acc(op_equals(Id, Args, Program), Input, Output) 
   end.
 
 part1() ->
   Program = intcode:get_program("inputs/day5.txt"), 
-  run_program(Program).
+  run_program(Program, 1).
+
+part2() ->
+  Program = intcode:get_program("inputs/day5.txt"),
+  run_program(Program, 5).
